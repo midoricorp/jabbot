28d4db2454407f205066d94ddbd3ff3e0990feaa
vmorsiani <vmorsiani@gmail.com>
2014-06-06 12:22:50 +0200

    Jira issue viewer for Jabbot

---
+++ jabbot-base-extensions/src/main/java/org/wanna/jabbot/command/IssueViewer.java
@@ -0,0 +1,90 @@
+package org.wanna.jabbot.command;
+
+import com.atlassian.jira.rest.client.JiraRestClient;
+import com.atlassian.jira.rest.client.NullProgressMonitor;
+import com.atlassian.jira.rest.client.RestClientException;
+import com.atlassian.jira.rest.client.domain.Issue;
+import com.atlassian.jira.rest.client.internal.jersey.JerseyJiraRestClientFactory;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author vmorsiani <vmorsiani>
+ * @since 2014-06-04
+ */
+public class IssueViewer extends AbstractCommand{
+	final Logger logger = LoggerFactory.getLogger(IssueViewer.class);
+	private String username, password;
+	private String jiraUrl;
+
+	@Override
+	public String getCommandName() {
+		return "jira";
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setJiraUrl(String jiraUrl) {
+		this.jiraUrl = jiraUrl;
+	}
+
+	@Override
+	public void process(MucHolder chatroom, Message message) throws XMPPException, SmackException.NotConnectedException {
+		String key = getParsedCommand().getArgs()[0];
+		try {
+			Issue issue = retrieveIssue(key);
+			StringBuilder sb = new StringBuilder();
+			if(issue != null ){
+				sb.append("[").append(issue.getKey()).append("] ");
+				sb.append(issue.getSummary()).append("\n");
+				sb.append("Reporter: ").append((issue.getReporter() != null ? issue.getReporter().getDisplayName() : "UNKNOWN")).append("\n");
+				sb.append("Assignee: ").append(issue.getAssignee() != null ? issue.getAssignee().getDisplayName() : "NONE").append("\n");
+				sb.append("Status: ").append(issue.getStatus().getName()).append("\n");
+				if(issue.getResolution() != null ){
+					sb.append("Resolution: ").append(issue.getResolution().getName());
+				}
+			}else{
+				sb.append("unable to find issue with key '").append(key).append("'");
+			}
+
+			chatroom.getMuc().sendMessage(sb.toString());
+		} catch (Exception e) {
+			logger.error("error while retrieving issue",e);
+		}
+	}
+
+	@Override
+	public void process(Message message) {
+
+	}
+
+	private com.atlassian.jira.rest.client.domain.Issue retrieveIssue(String key) throws URISyntaxException, InterruptedException {
+		final JerseyJiraRestClientFactory factory = new JerseyJiraRestClientFactory();
+		final URI jiraServerUri = new URI(jiraUrl);
+		final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, username, password);
+		final NullProgressMonitor pm = new NullProgressMonitor();
+		try{
+			final com.atlassian.jira.rest.client.domain.Issue issue = restClient.getIssueClient().getIssue(key, pm);
+			logger.trace(issue.toString());
+			return issue;
+
+		}catch(RestClientException e){
+			logger.error("rest exception ",e);
+			return null;
+		}
+
+	}
+
+}
