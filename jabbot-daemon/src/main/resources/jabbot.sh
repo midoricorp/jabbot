5b78dbf7041eebe56c581cebb91a5c626ec43d39
vmorsiani <vmorsiani@gmail.com>
2014-06-19 23:20:42 +0200

    added file logger & log dir

--- jabbot-daemon/src/main/resources/jabbot.sh
+++ jabbot-daemon/src/main/resources/jabbot.sh
@@ -1,18 +1,16 @@
 #!/bin/sh 
 
-
-
 daemon_start(){
   echo "starting.."
   echo "JABBOT_HOME: "$JABBOT_HOME
   echo "JAVA_HOME: "$JAVA_HOME
   cd $JABBOT_HOME"/lib"
-  exec jsvc  -cp "./*" -pidfile $JABBOT_HOME"/jabbot.pid" -debug -home $JAVA_HOME $JABBOT_MAIN
+  exec jsvc  -cp "./*" -Djabbot.logs_dir=$JABBOT_LOGS -pidfile $JABBOT_HOME"/jabbot.pid" -debug -home $JAVA_HOME $JABBOT_MAIN
 }
 
 daemon_stop(){
   cd $JABBOT_HOME"/lib"
-  exec jsvc -stop -cp "./*" -pidfile $JABBOT_HOME"/jabbot.pid" -debug -home $JAVA_HOME $JABBOT_MAIN
+  exec jsvc -stop -cp "./*" -Djabbot.logs_dir=$JABBOT_LOGS -pidfile $JABBOT_HOME"/jabbot.pid" -debug -home $JAVA_HOME $JABBOT_MAIN
 }
 
 
@@ -26,6 +24,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 JABBOT_HOME=$(dirname "$SCRIPTPATH")
 JABBOT_MAIN="org.wanna.jabbot.Launcher"
+JABBOT_LOGS=$JABBOT_HOME"/logs"
 
 case $1 in
 start)
