67f295bbd05619828d925a1ae650c03d0cbe05bb
Vincent Morsiani <vmorsiani@gmail.com>
2015-03-04 10:09:46 +0100

    javadoc

--- jabbot-extension-api/src/main/java/org/wanna/jabbot/command/behavior/MessageSenderAware.java
+++ jabbot-extension-api/src/main/java/org/wanna/jabbot/command/behavior/MessageSenderAware.java
@@ -3,9 +3,17 @@ package org.wanna.jabbot.command.behavior;
 import org.wanna.jabbot.command.MessageSender;
 
 /**
+ * Interface which indicates that the implementor is MessageSender aware.
+ * Any commands implementing this interface will have a MessageSender implementation injected
+ * which will allow it to autonomously send message.
+ * 
  * @author vmorsiani <vmorsiani>
  * @since 2015-02-24
  */
 public interface MessageSenderAware {
+	/**
+	 * Setter for the MessageSender
+	 * @param messageSender the message sender to be injected
+	 */
 	void setMessageSender(MessageSender messageSender);
 }
