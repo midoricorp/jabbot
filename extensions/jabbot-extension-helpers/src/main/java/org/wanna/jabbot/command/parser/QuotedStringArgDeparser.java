1025396256b7c067b656fcc6f3ef53097688db81
Torrey Searle <tsearle@gmail.com>
2015-03-18 22:38:08 +0100

    Add multi-arg support for invoking midori commands allow midori-script functions to be registerd as commands

---
+++ extensions/jabbot-extension-helpers/src/main/java/org/wanna/jabbot/command/parser/QuotedStringArgDeparser.java
@@ -0,0 +1,25 @@
+package org.wanna.jabbot.command.parser;
+
+import  java.util.List;
+
+/**
+ * pretty much all commands use Quoted String for arg parsing
+ * if you want to pass args to such a command use this class to turn
+ * your param list back into a string, this class needs to be made
+ * more robust as the corresponding QuotedStringArgParser Is
+ *
+ * @author tsearle
+ */
+public class QuotedStringArgDeparser {
+
+	public static String deparse(List<String> args) {
+		StringBuffer sb = new StringBuffer();
+
+		for (String arg : args) {
+			sb.append("\"");
+			sb.append(arg);
+			sb.append("\" ");
+		}
+		return sb.toString();
+	}
+}
---
+++ extensions/jabbot-script-command/src/main/java/org/wanna/jabbot/extensions/script/ScriptScript.java
@@ -0,0 +1,64 @@
+package org.wanna.jabbot.extensions.script;
+
+import org.wanna.jabbot.command.*;
+import org.wanna.jabbot.command.parser.ArgsParser;
+import org.wanna.jabbot.command.parser.QuotedStringArgParser;
+import com.sipstacks.script.ScriptParseException;
+import java.util.List;
+
+/**
+ * @author vmorsiani <vmorsiani>
+ * @since 2014-05-30
+ */
+public class ScriptScript implements Command {
+
+	com.sipstacks.script.Command scriptCmd;
+	String name;
+
+	public ScriptScript(String name, com.sipstacks.script.Command scriptCmd) {
+		this.name = name;
+		this.scriptCmd = scriptCmd;
+	}
+
+        @Override
+        public ArgsParser getArgsParser() {
+                return new QuotedStringArgParser();
+        }
+
+	public String getCommandName() {
+		return name;
+	}
+
+	/**
+	 * Returns a short description of the command
+	 * @return String command description
+	 */
+	public String getDescription() { return null;}
+
+	/**
+	 * Returns the command help
+	 * @return String help message
+	 */
+	public String getHelpMessage() { return null;}
+
+	public CommandMessage process(CommandMessage message) {
+		String argsString = message.getBody();
+
+		List<String> args = getArgsParser().parse(argsString);
+		String response;
+
+		try {
+			response = scriptCmd.exec(args);
+		} catch (ScriptParseException e) {
+			DefaultCommandMessage result = new DefaultCommandMessage();
+			result.setBody(e.getMessage());
+			return result;
+
+		}
+
+		DefaultCommandMessage result = new DefaultCommandMessage();
+		result.setBody(response);
+		return result;
+	}
+}
+
