e8b8ccbcc7022d007138ae00188c453246cc4960
Torrey Searle <tsearle@gmail.com>
2015-04-06 13:46:56 +0200

    fix translate extension add "type" to serverList

--- genconf.pl
+++ genconf.pl
@@ -43,8 +43,13 @@ sub ask_multi {
 	my $value;
 
 	if (defined $save_value) {
-		print "@$save_value\n";
-		$value=$save_value;
+		if (ref($save_key) eq 'ARRAY') {
+			print "@$save_value\n";
+			$value=$save_value;
+		} else {
+			print "$save_value\n";
+			$value=[$save_value];
+		}
 	} else {
 		my $answer = <>;
 		chomp($answer);
@@ -57,9 +62,6 @@ sub ask_multi {
 
 		$value = \@answer_list;
 
-		if (defined $save_key) {
-			$saved_values->param($save_key, $value);
-		}
 	}
 
 	return $value;
@@ -178,6 +180,8 @@ sub makeServer {
 	my $serverConfig = { 'debug' => "false", "commandPrefix"=>"!" };
 
 	my $type = shift;
+
+	$serverConfig->{'type'} = $type;
 
 	my $binding;
 	foreach $b (@binding_templates) {
