e503c5cc3e266c91e12a8ebd1dee0d314dde9db6
Torrey Searle <tsearle@gmail.com>
2020-05-30 10:57:01 +0200

    attempt to make Discord parser more robust

--- bindings/jabbot-discord-binding/src/main/java/org/wanna/jabbot/binding/discord/DiscordBinding.java
+++ bindings/jabbot-discord-binding/src/main/java/org/wanna/jabbot/binding/discord/DiscordBinding.java
@@ -24,6 +24,8 @@ import org.wanna.jabbot.messaging.body.BodyPart;
 
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author vmorsiani <vmorsiani>
@@ -108,14 +110,79 @@ public class DiscordBinding extends AbstractBinding<Object> {
 		return null;
 	}
 
+	private String escapeMarkdown(String origMessage) {
+		StringBuffer sb = new StringBuffer();
+		final String HTTP = "http://";
+		final String HTTPS = "https://";
+		final Pattern url = Pattern.compile("(https?:\\/\\/[-a-zA-Z0-9@:%._\\+~#=?;]+)");
+		boolean inQuote = false;
+		for(int i = 0; i < origMessage.length();i++) {
+			// only escape urls if in quoted strings
+			if (inQuote) {
+				char c = origMessage.charAt(i);
+				if (c == '"') {
+					inQuote = false;
+					sb.append('"');
+					continue;
+				}else if (origMessage.startsWith(HTTP)) {
+
+					sb.append("<");
+					sb.append(HTTP);
+					sb.append(">");
+					i+=(HTTP.length()-1);
+				}else if (origMessage.startsWith(HTTPS)) {
+
+					sb.append("<");
+					sb.append(HTTPS);
+					sb.append(">");
+					i+=(HTTPS.length()-1);
+				} else {
+					switch(c) {
+						case '_':
+						case '*':
+						case '~':
+							sb.append("\\");
+							sb.append(c);
+							break;
+						default:
+							sb.append(c);
+					}
+				}
+			} else {
+				char c = origMessage.charAt(i);
+				switch(c) {
+					case '_':
+					case '*':
+					case '~':
+						sb.append("\\");
+						sb.append(c);
+						break;
+					case 'h':
+						Matcher m = url.matcher(origMessage);
+						if(m.find(i)) {
+							if (m.start() == i) {
+								sb.append(origMessage.substring(m.start(),m.end()));
+								break;
+							}
+						}
+					default:
+						sb.append(c);
+				}
+
+			}
+		}
+		return sb.toString();
+	}
+
 	@Override
 	public void sendMessage(TxMessage response) {
 		MessageContent messageContent = response.getMessageContent();
 		DiscordResource resource = (DiscordResource)response.getRequest().getSender();
 
 		String message = messageContent.getBody(BodyPart.Type.TEXT).getText();
+		//message = message.replaceAll("([*_~])", "\\\\$1");
+		message = escapeMarkdown(message);
 		logger.info("Sending message: " + message);
-		message = message.replaceAll("([*_~])", "\\\\$1");
 		resource.channel.sendMessage(message);
 	}
 
